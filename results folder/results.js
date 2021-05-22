1e716d47c44441dfcea6ba374803e92fd7576dae
Brandon Clark <clarkwbrandon2@gmail.com>
2021-05-21 22:03:29 -0700

    fixed API issue

--- results folder/results.js
+++ results folder/results.js
@@ -1,20 +1,20 @@
 document.getElementById('searchGames').addEventListener('click', event =>{
   event.preventDefault()
   console.log('ping')
-})
+
 
 axios.get('https://cors-proxy-j.herokuapp.com/', {
   headers: {
     
-    'Target-URL': `//api.rawg.io/api/platforms?key=2c28abeb9697423fbf38597eb19afcd2&search=${document.getElementById('title').value}`
+    'Target-URL': `https://api.rawg.io/api/games?key=2c28abeb9697423fbf38597eb19afcd2&search=${document.getElementById('title').value}`
 ,
     
     'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
   }
 })
-  .then(({ data }) => {
+  .then( res => {
     
-    let videoGames = data
+    let videoGames = res
     console.log(videoGames)
     document.getElementById('videoGames').innerHTML = ''
    
@@ -22,4 +22,4 @@ axios.get('https://cors-proxy-j.herokuapp.com/', {
   })
   .catch(err => console.error(err))
 
- 
+})
