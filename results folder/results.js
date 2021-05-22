0335ed47c77240e3b7922ce35ab837ac1813fb76
Brandon Clark <clarkwbrandon2@gmail.com>
2021-05-21 21:35:57 -0700

    api key not working

--- results folder/results.js
+++ results folder/results.js
@@ -1,15 +1,23 @@
+document.getElementById('searchGames').addEventListener('click', event =>{
+  event.preventDefault()
+  console.log('ping')
+})
 
 axios.get('https://cors-proxy-j.herokuapp.com/', {
   headers: {
     
-    'Target-URL': "https://api.rawg.io/api/games?key=2c28abeb9697423fbf38597eb19afcd2&search=stardew valley"
+    'Target-URL': `//api.rawg.io/api/platforms?key=2c28abeb9697423fbf38597eb19afcd2&search=${document.getElementById('title').value}`
 ,
     
     'Authorization': 'Bearer lbogapYHxff9h2fSNoWEoM420b8mRfQ4JBsiphR6BtaNKlmR51XQt3wCm2ocKhlkvpnv_46BvAcMuB_cTrv7pmRtuMMplxzaBAA_nAU57ttpRZlv9y05lvxWcXUoX3Yx'
   }
 })
   .then(({ data }) => {
-    console.log(data)
+    
+    let videoGames = data
+    console.log(videoGames)
+    document.getElementById('videoGames').innerHTML = ''
+   
 
   })
   .catch(err => console.error(err))
